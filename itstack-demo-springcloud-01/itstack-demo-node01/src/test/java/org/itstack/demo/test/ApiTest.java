0c1c13e470a161a46d996d768f959ddd540e969b
fuzhengwei <184172133@qq.com>
2019-11-07 08:28:51 +0800

    微信公众号：bugstack虫洞栈 | Spring Cloud(一)《服务集群注册与发现 Eureka》

---
+++ itstack-demo-springcloud-01/itstack-demo-node01/src/test/java/org/itstack/demo/test/ApiTest.java
@@ -0,0 +1,14 @@
+package org.itstack.demo.test;
+
+/**
+ * 微信公众号：bugstack虫洞栈 | 沉淀、分享、成长，专注于原创专题案例
+ * 论坛：http://bugstack.cn
+ * Create by 付政委 on @2019
+ */
+public class ApiTest {
+
+    public static void main(String[] args) {
+        System.out.println("hi 微信公众号：bugstack虫洞栈");
+    }
+
+}
